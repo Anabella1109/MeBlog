aee73b8a09cbefc7453926f57ac70588579bc401
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-03-01 12:25:24 +0200

    add post and comment form in main/forms.py

---
+++ app/main/forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
+from wtforms.validators import Required
+
+class AddPostForm(FlaskForm):
+    title=StringField('Title',validators = [Required()])
+    content=TextAreaField('Content',validators = [Required()])
+    submit=SubmitField('SUBMIT')
+
+class CommentForm(FlaskForm):
+   
+   username = StringField('Enter your name',validators=[Required()])
+   comment = TextAreaField('pitch comment', validators=[Required()])
+   submit = SubmitField('Submit')
