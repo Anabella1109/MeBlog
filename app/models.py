c3243c6690cd17460852242be4a7ee1444bedf5c
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-03-01 09:35:32 +0200

    add Quote class in models.py

---
+++ app/models.py
@@ -0,0 +1,10 @@
+class Quote:
+   '''
+   Quote class to define quote objects
+   '''
+
+   def __init__(self,id,author,content):
+     self.id=id
+     self.author=author
+     self.content=content
+    
